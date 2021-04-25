b2bcaf6fa1d07a998b2082229f2e4ca71f2db89f
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-04-24 20:18:22 -0400

    Remove vowels from String solution2

--- Remove Vowels from a String/Solution.java
+++ Remove Vowels from a String/Solution.java
@@ -1,5 +1,22 @@
+// Solution 1
 class Solution {
     public String removeVowels(String s) {
        return s.replaceAll("[aeiou]", "");
+    }
+}
+
+// Solution 2
+class Solution {
+    public String removeVowels(String s) {
+       StringBuilder sb = new StringBuilder();
+        for(int i=0; i< s.length(); i++){
+            char c = s.charAt(i);
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+                
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
     }
 }
