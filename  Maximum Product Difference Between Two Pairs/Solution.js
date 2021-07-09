f2daeebc9ded99feceddce250cad8c9325d4cce8
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-07-08 20:34:07 -0400

    Maximum Product Difference Between Two Pairs js solution 2

---  Maximum Product Difference Between Two Pairs/Solution.js
+++  Maximum Product Difference Between Two Pairs/Solution.js
@@ -8,3 +8,31 @@ var maxProductDifference = function(nums) {
     let n = nums.length
     return (nums[n-1] * nums[n-2]) - (nums[0] * nums[1])
 };
+
+//Solution 2
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProductDifference = function(nums) {
+    let first = 0;
+    let second = 0;
+    
+    let third = Infinity
+    let fourth = Infinity
+    
+    for(let i= 0; i< nums.length; i++) {
+        let value = nums[i]
+        
+        if(value >= first || value >= second) {
+            second = first > second ? first : second
+            first = value
+        }
+        
+        if(value <= third || value <= fourth) {
+            fourth = third < fourth ? third : fourth
+            third = value
+        }
+    }
+    return (first*second) - (third*fourth)
+};
