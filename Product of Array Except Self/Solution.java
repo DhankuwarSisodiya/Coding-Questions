f7c9fe7fcf92f1e49931b95c595b91ce51832753
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-08-17 19:23:10 -0400

    Product of Array Except Self sol 2

--- Product of Array Except Self/Solution.java
+++ Product of Array Except Self/Solution.java
@@ -18,3 +18,28 @@ class Solution {
         return answer;
     }
 }
+
+//Solution2 
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] answer = new int[length];
+        //[1,2,3,4]
+        answer[0] = 1;
+        for (int i = 1; i < length; i++) {
+            answer[i] = nums[i - 1] * answer[i - 1];
+            System.out.println(answer[i]);
+        }
+        //[1,1,2,6]
+        
+        int R = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            answer[i] = answer[i] * R;
+            System.out.println(answer[i]);
+            R *= nums[i];
+            System.out.println(R);
+        }
+        //[,6,1]
+        return answer;
+    }
+}
