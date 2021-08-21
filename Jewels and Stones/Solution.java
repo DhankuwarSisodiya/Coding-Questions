94278fa7bcacc04ad20ac1714500d60cd2a809aa
Dhankuwar <dhankuwarsisodiya@gmail.com>
2021-08-21 12:59:29 -0400

    Jewels & stones java sol

---
+++ Jewels and Stones/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        HashSet<Character> set = new HashSet<Character>();
+        for (int i = 0; i< jewels.length(); i++) {
+            char c = jewels.charAt(i);
+            set.add(c);
+        }
+        for (int i = 0; i< stones.length(); i++) {
+            char c = stones.charAt(i);
+            if(set.contains(c)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
